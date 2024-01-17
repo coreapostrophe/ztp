85243f3732d901e17cc81fd50347e3ac181d2b29
Core <daverencordero@gmail.com>
2024-01-17 10:28:24 +0800

    test: added request body format guard

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -1,10 +1,7 @@
 use std::net::TcpListener;
 
 use actix_web::{dev::Server, web, App, HttpResponse, HttpServer, Responder};
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().finish()
-}
+use serde::Deserialize;
 
 pub enum BindOption<'a> {
     SocketAddr((&'a str, u16)),
@@ -24,8 +21,11 @@ impl<'a> Into<BindOption<'a>> for TcpListener {
 }
 
 pub fn run<'a>(option: impl Into<BindOption<'a>>) -> std::io::Result<Server> {
-    let http_server =
-        HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)));
+    let http_server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    });
 
     let server = match option.into() {
         BindOption::SocketAddr(address) => http_server.bind(address)?.run(),
@@ -33,4 +33,18 @@ pub fn run<'a>(option: impl Into<BindOption<'a>>) -> std::io::Result<Server> {
     };
 
     Ok(server)
+}
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+#[derive(Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
+    HttpResponse::Ok().finish()
 }
