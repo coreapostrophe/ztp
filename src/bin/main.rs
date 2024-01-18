595f2090c8fb6492e53a24079923e22a050599e0
coreapostrophe <daverencordero@gmail.com>
2024-01-18 18:30:40 +0800

    feat: added configuration

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,12 @@
-use ztplib::run;
+use std::net::TcpListener;
+
+use ztplib::{configuration::get_configuration, startup::ZtpServer};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run(("127.0.0.1", 8080))?.await
+    let config = get_configuration().expect("to read configuration");
+    let address = format!("127.0.0.1:{}", config.application_port);
+    let listener = TcpListener::bind(address)?;
+
+    ZtpServer::run(listener)?.await
 }
