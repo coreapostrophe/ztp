59efd5b8018ee8da4944d428552ea6cd6d472bab
coreapostrophe <daverencordero@gmail.com>
2024-01-25 19:10:35 +0800

    fix: resolved clippy error

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -10,7 +10,7 @@ async fn main() -> std::io::Result<()> {
     ZtpTelemetry::init_subscriber(subscriber);
 
     let config = ZtpConfiguration::get_configuration().expect("Failed to read configuration");
-    let connection_pool = PgPool::connect(&config.database.connection_string().expose_secret())
+    let connection_pool = PgPool::connect(config.database.connection_string().expose_secret())
         .await
         .expect("to connect to Postgres.");
     let listener = TcpListener::bind(("127.0.0.1", 8080))?;
